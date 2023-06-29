6727b2b2a971d37adeb547378fb52e7fcdca8355
robstowe <robstowe94@gmail.com>
2023-06-29 16:04:41 -0700

    framework for comments done

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -73,6 +73,7 @@ router.get('/login', (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
     res.redirect('/homepage'); //changed from profile
+    console.log(user.id, 'is my id');
     return;
   }
 
--- nbaApi/script.js
+++ nbaApi/script.js
@@ -27,22 +27,6 @@ fetch(schedulesBasic)
   });
 
 
-=======
-var nbaAPI = "https://api.sportsdata.io/v3/nba/scores/json/GamesByDate/2023-06-01?key=49d03c3b1c98473d9319c7319ab5f1dd";
 
 
-function gatherAPI() {
-    // event.preventDefault();
-    console.log(nbaAPI);
-    fetch(nbaAPI)
-        .then(function (response) {
-            console.log(response.json());
-            // return response.json();
-            return;
-            
-        }); 
-   
-}
-
-gatherAPI();
 
