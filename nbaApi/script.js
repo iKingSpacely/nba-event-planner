1913515e62643dc6387f3cf07e98eb7556b143c3
Suraj <snair306@gmail.com>
2023-07-03 16:38:30 -0500

    got basic data displaying

--- nbaApi/script.js
+++ nbaApi/script.js
@@ -1,35 +1,40 @@
-const schedulesBasic = "https://api.sportsdata.io/v3/nba/scores/json/SchedulesBasic/2022?key=49d03c3b1c98473d9319c7319ab5f1dd";
 
 
-fetch(schedulesBasic)
-.then(response => response.json())
-  .then(data => {
-    // Extract date, home team, and away team information
-    const games = data.map(game => {
-      const date = game.DateTime;
-      const day = game.Day;
-      const homeTeam = game.HomeTeam;
-      const homeScore = game.HomeTeamScore; 
-      const awayTeam = game.AwayTeam;
-      const awayScore = game.AwayTeamScore; 
+// const schedulesBasic = "https://api.sportsdata.io/v3/nba/scores/json/SchedulesBasic/2022?key=49d03c3b1c98473d9319c7319ab5f1dd";
+
+
+// fetch(schedulesBasic)
+// .then(response => response.json())
+//   .then(data => {
+//     // Extract date, home team, and away team information
+//     const games = data.map(game => {
+//       const date = game.DateTime;
+//       const day = game.Day;
+//       const homeTeam = game.HomeTeam;
+//       const homeScore = game.HomeTeamScore; 
+//       const awayTeam = game.AwayTeam;
+//       const awayScore = game.AwayTeamScore; 
       
-      return { date, day, homeTeam, homeScore, awayTeam, awayScore };
-    });
+//       return { date, day, homeTeam, homeScore, awayTeam, awayScore };
+//     });
 
-    // Do something with the extracted information
-    console.log(games);
-    const teamGames = games.filter(game => (game.homeTeam === "MIL" || game.awayTeam === "MIL"))
-    let searchBody = document.createElement('div');
-    let homeTeam = document.createElement('p');
-    let awayTeam = document.createElement('p');
-    let homeScore = document.createElement('p');
-    let awayScore = document.createElement('p');
-    let date = document.createElement('p');
-    let day = document.createElement('p');
-    console.log(teamGames);
-  })
-  .catch(error => {
-    console.log('Error:', error);
-  });
+//     // Do something with the extracted information
+//     console.log(games);
+//     const teamGames = games.filter(game => (game.homeTeam === "MIL" || game.awayTeam === "MIL"))
+//     let searchBody = document.createElement('div');
+//     let homeTeam = document.createElement('p');
+//     let awayTeam = document.createElement('p');
+//     let homeScore = document.createElement('p');
+//     let awayScore = document.createElement('p');
+//     let date = document.createElement('p');
+//     let day = document.createElement('p');
+//     console.log(teamGames);
+//   })
+//   .catch(error => {
+//     console.log('Error:', error);
+//   });
 
 
+// module.exports = { 
+//   games: games 
+// };
