bb6c204a7618c27ebef106796d201c2f77aa1b44
Spacely <accolin89@gmail.c>
2023-07-04 13:42:59 -0500

    fixed results and commented out bootstrap body from hompeage

--- models/Results.js
+++ models/Results.js
@@ -2,68 +2,57 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 
-class Search extends Model { };
+class Results extends Model { }
 
-Search.init({
-
-class Results extends Model { };
-
-Results.init({
-
-    id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        primaryKey: true,
-        autoIncrement: true,
-    },
-    day: {
-        type: DataTypes.DATE,
-        allowNull: true,
-    },
-    date_time: {
-        type: DataTypes.DATE,
-        allowNull: true,
-    },
-    away_team: {
-        type: DataTypes.STRING,
-        allowNull: true,
-    },
-    home_team: {
-        type: DataTypes.STRING,
-        allowNull: true,
-    },
-    away_score: {
-        type: DataTypes.INTEGER,
-        allowNull: true,
-    },
-    home_score: {
-        type: DataTypes.INTEGER,
-        allowNull: true,
-    },
-    user_id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        references: {
-            model: 'user',
-            key: 'id',
+Results.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
         },
-
+        day: {
+            type: DataTypes.DATE,
+            allowNull: true,
+        },
+        date_time: {
+            type: DataTypes.DATE,
+            allowNull: true,
+        },
+        away_team: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        home_team: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        away_score: {
+            type: DataTypes.INTEGER,
+            allowNull: true,
+        },
+        home_score: {
+            type: DataTypes.INTEGER,
+            allowNull: true,
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'user',
+                key: 'id',
+            },
+        },
     },
-
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'results',
+    },
     // day,dattime,away, home team, scores for both,
-});
-
-module.exports = Search;
-
-    }
-},
-{
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'results',
-  }
 );
 
 module.exports = Results;
