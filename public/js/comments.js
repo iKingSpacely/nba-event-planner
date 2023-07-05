8bf4814c9275bfd46a8b6c73f75d0fb96e06ee83
robstowe <robstowe94@gmail.com>
2023-07-05 10:01:14 -0700

    comments

--- public/js/comments.js
+++ public/js/comments.js
@@ -4,34 +4,55 @@ const newComment = async (event) => {
     event.preventDefault();
 
     const comment = document.querySelector('input[name="post-id"]').value;
-}
-
-
-
-
-
-const commentFormHandler = async function(event) {
-    event.preventDefault();
-  
-    const postId = document.querySelector('input[name="post-id"]').value;
     const body = document.querySelector('textarea[name="comment-body"]').value;
-  
+
     if (body) {
-      await fetch('/api/comment', {
-        method: 'POST',
-        body: JSON.stringify({
-          postId,
-          body
-        }),
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      });
+        await fetch('/api/comments', {
+          method: 'POST',
+          body: JSON.stringify({
+            comment,
+            body
+          }),
+          headers: {
+            'Content-Type': 'application/json'
+          }
+          
+        });
+        console.log(body);
+        document.location.reload();
+      }
+
+};
+
+document
+.querySelector('new-comment-form')
+.addEventListener('btn btn-success','btn btn-danger', newComment);
+
+
+
+
+// const commentFormHandler = async function(event) {
+//     event.preventDefault();
   
-      document.location.reload();
-    }
-  };
+//     const postId = document.querySelector('input[name="post-id"]').value;
+//     const body = document.querySelector('textarea[name="comment-body"]').value;
   
-  document
-    .querySelector('new-comment-form')
-    .addEventListener('btn btn-success','btn btn-danger', commentFormHandler);
+//     if (body) {
+//       await fetch('/api/comment', {
+//         method: 'POST',
+//         body: JSON.stringify({
+//           postId,
+//           body
+//         }),
+//         headers: {
+//           'Content-Type': 'application/json'
+//         }
+//       });
+  
+//       document.location.reload();
+//     }
+//   };
+  
+//   document
+//     .querySelector('new-comment-form')
+//     .addEventListener('btn btn-success','btn btn-danger', commentFormHandler);
